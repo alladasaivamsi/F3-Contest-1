89519865b1f7996684409f69988e5ab1b22ceec4
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-02-11 19:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,29 @@ fetch("https://dummyjson.com/products")
 
     //reading the response
     .then((data) => {
+        
         console.log(data.products);
     
         let myArr = data.products;
         let productData = document.getElementById("shop");
 
         // adding the content of data to a shopping cart by using the map function
-        // all the data of title , brand , price , rating , stock is used to store and add the data to their particular position.
+        // all the data of title , brand , price , rating , stock , description , discount is used to store and add the data to their particular position.
 
         myArr.map((products) =>
         {
             productData.innerHTML += `<div id="data"> 
-                                            <img src=${products.images[0]} alt="images" height="220px" width="219px">
+                                            <img src=${products.images[0]} alt="images" id="imageData">
                                             <div id="content-data"> 
+                                                <p><strong>Category: </strong><span id="category">${products.category}</span></p>
+                                                <p><strong>Id: </strong><span id="id">${products.id}</span></p>
                                                 <p><strong>Title: </strong><span id="title">${products.title}</span></p>
                                                 <p><strong>Brand: </strong><span id="brand">${products.brand}</span></p>
                                                 <p><strong>Price: </strong><span id="price">$${products.price}</span></p>
                                                 <p><strong>Rating: </strong><span id="rating">${products.rating}</span></p>
                                                 <p><strong>Stock: </strong><span id="stock">${products.stock}</span></p>
+                                                <p><strong>Description: </strong><p id="description">${products.description}</p></p>
+                                                <p><strong>Discount Percentage: </strong><span id="discount">${products.discountPercentage} %</span></p>
                                             </div>  
                                         </div>`;
         })      
